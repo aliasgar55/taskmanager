da8963198b6cf39a6de1f2b893620822d9e9c4ab
joancena <joancena1268@gmail.com>
2020-12-10 20:24:23 +0530

    refactored index.js to use async and await and added update operation for user and task

--- src/index.js
+++ src/index.js
@@ -2,43 +2,123 @@ const express = require("express");
 require("./db/mongoose");
 const User = require("./models/User");
 const Task = require("./models/Task");
+const { update } = require("./models/User");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/user", (req, res) => {
+app.post("/user", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then((result) => res.status(201).send(result))
-    .catch((e) => res.status(400).send(e));
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.post("/task", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then((result) => res.status(201).send(result))
-    .catch((e) => res.status(400).send(e));
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
-app.get("/user", (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((e) => res.status(500));
-});
-
-app.get("/user/:id", (req, res) => {
+app.get("/user/:id", async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      return res.send(user);
-    })
-    .catch((e) => res.status(500).send());
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const updates = Object.keys(req.body);
+  const isValidUpdate = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Invalid update!" });
+  }
+  const _id = req.params.id;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).send({ error: "user not found" });
+    }
+    res.send(updatedUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.post("/task", async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get("/task", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.get("/task/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send("Task not found");
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.patch("/task/:id", async (req, res) => {
+  const _id = req.params.id;
+  const allowedUpdates = ["description", "completed"];
+  const requestedUpdates = Object.keys(req.body);
+  const isValidUpdate = requestedUpdates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Invalid Update" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
