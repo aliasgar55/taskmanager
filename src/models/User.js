bc7c24065267368b724c20fb00ae1b3a9fa4342b
joancena <joancena1268@gmail.com>
2020-12-12 23:02:54 +0530

    added web tokens to the project

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,8 +42,22 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
-
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thequickbrownfox");
+  user.tokens = user.tokens.concat({ token: token });
+  await user.save();
+  return token;
+};
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user) {
--- src/routes/user.js
+++ src/routes/user.js
@@ -7,6 +7,7 @@ router.post("/user", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
+    const token = await user.generateAuthToken();
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
@@ -17,9 +18,10 @@ router.post("/user/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
     res.send(user);
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).send();
   }
 });
 
